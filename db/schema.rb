6bec6dd534a5d2364ad1e22fbd4df6ef00f90a6c
tetsuya-omoto <tetsuya1329@gmail.com>
2019-12-17 23:31:29 +0900

    top_view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_073727) do
+ActiveRecord::Schema.define(version: 2019_12_17_074355) do
+
+  create_table "jobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
